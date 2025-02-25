62e8b43847db0a263aa53a55cb3f4536c5c70c44
alanaainez <alainez649@gmail.com>
2025-02-25 13:50:42 -0500

    git commit -m "Foundation for code"

---
+++ Main/dist/controllers/thought-controller.js
@@ -0,0 +1,8 @@
+"use strict";
+// get all thoughts
+// get single thought by id
+// create a thought
+// update thought
+// delete thought
+// add a reaction to a thought
+// remove reaction from a thought
